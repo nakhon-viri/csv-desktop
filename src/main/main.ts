c9e41102e8661bb56b0ff658f6162e1f857df8c6
NNN <nkn.vrw@gmail.com>
2024-01-31 20:37:04 +0700

    first commit

---
+++ src/main/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import { db } from "./db/index";
+import { test } from "./db/schema/schema";
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({});
+  mainWindow.webContents.openDevTools();
+  // Vite dev server URL
+  mainWindow.loadURL("http://localhost:5173");
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow == null) {
+    createWindow();
+  }
+});
+
+const load = async () => {
+  const result = await db.insert(test).values({
+    id: 7,
+  });
+  console.log("result", result);
+};
+// load();
